9e719a32ee2b79cdcb9acd72e106532d2eb06c3a
sai <saisri.sizzlers09@gmail.com>
2019-12-28 21:28:33 -0500

    trigger changes

--- devops.py
+++ devops.py
@@ -7,7 +7,4 @@ months = currentage*12
 days = currentage*365
 print("hello "+name+" .you are %d years old." % (currentage))
 print("hello "+name+" .you are %d months old." % (months))
-def aad():
-    num = input ("please enter you aad number")
-    print(num)
-    aad()
+
